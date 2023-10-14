69c4e0bc044b15641751d5d76e2b7f44baa5e74f
Jiwon <jiwon_yoo@brown.edu>
2023-10-13 23:33:07 -0400

    committing to pull

--- mock/tests/App.spec.ts
+++ mock/tests/App.spec.ts
@@ -1,16 +1,14 @@
 import { test, expect } from "@playwright/test";
 
-test.beforeEach(() => {
-  // TODO: Is there something we need to do before every test case to avoid repeating code?
+test.beforeEach("go to the local host", async ({ page }) => {
+  await page.goto("http://localhost:8000/");
 });
 
 test("on page load, i see an input bar", async ({ page }) => {
-  await page.goto("http://localhost:8000/");
   await expect(page.getByLabel("Command input")).toBeVisible();
 });
 
 test("after I type into the input box, its text changes", async ({ page }) => {
-  await page.goto("http://localhost:8000/");
   await page.getByLabel("Command input").click();
   await page.getByLabel("Command input").fill("Awesome command");
   const mock_input = `Awesome command`;
@@ -18,19 +16,16 @@ test("after I type into the input box, its text changes", async ({ page }) => {
 });
 
 test("on page load, i see a button", async ({ page }) => {
-  await page.goto("http://localhost:8000/");
   await expect(page.getByRole("button")).toBeVisible();
 });
 
 test("the default mode is brief", async ({ page }) => {
-  await page.goto("http://localhost:8000/");
   await expect(
     page.getByRole("button", { name: "Submitted Briefly" })
   ).toBeVisible();
 });
 
 test("mode switch changes the button", async ({ page }) => {
-  await page.goto("http://localhost:8000/");
   await expect(
     page.getByRole("button", { name: "Submitted Briefly" })
   ).toBeVisible();
@@ -46,35 +41,7 @@ test("mode switch changes the button", async ({ page }) => {
   ).toBeVisible();
 });
 
-test("load success message printing", async ({ page }) => {
-  await page.goto("http://localhost:8000/");
-  await page.getByPlaceholder("Enter command here!").fill("load people.csv");
-  await page.getByRole("button", { name: "Submitted Briefly" }).click();
-  await expect(
-    page.getByText("Output: Successfully loaded file people.csv")
-  ).toBeVisible();
-});
-
-test("test successful view", async ({ page }) => {
-  await page.goto("http://localhost:8000/");
-  await page.getByPlaceholder("Enter command here!").fill("load people.csv");
-  await page.getByRole("button", { name: "Submitted Briefly" }).click();
-  await page.getByPlaceholder("Enter command here!").fill("view people.csv");
-  await page.getByRole("button", { name: "Submitted Briefly" }).click();
-  //how to test for checking whether the table is there....
-});
-
-test("test view before load", async ({ page }) => {
-  await page.goto("http://localhost:8000/");
-  await page.getByPlaceholder("Enter command here!").fill("view people.csv");
-  await page.getByRole("button", { name: "Submitted Briefly" }).click();
-  await expect(
-    page.getByText("Output: Error: No dataset loaded")
-  ).toBeVisible();
-});
-
 test("test verbose mode", async ({ page }) => {
-  await page.goto("http://localhost:8000/");
   await page.getByPlaceholder("Enter command here!").fill("mode verbose");
   await page.getByRole("button", { name: "Submitted Briefly" }).click();
   await expect(
@@ -85,8 +52,31 @@ test("test verbose mode", async ({ page }) => {
   await expect(page.getByText("Output: Mode set to Brief")).toBeVisible();
 });
 
+test("load success message", async ({ page }) => {
+  await page.getByPlaceholder("Enter command here!").fill("load people.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await expect(
+    page.getByText("Output: Successfully loaded file people.csv")
+  ).toBeVisible();
+});
+
+test("view successful message", async ({ page }) => {
+  await page.getByPlaceholder("Enter command here!").fill("load people.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await page.getByPlaceholder("Enter command here!").fill("view people.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  //how to test for checking whether the table is there....
+});
+
+test("test view before load", async ({ page }) => {
+  await page.getByPlaceholder("Enter command here!").fill("view people.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await expect(
+    page.getByText("Output: Error: No dataset loaded")
+  ).toBeVisible();
+});
+
 test("test viewing not existing csv file", async ({ page }) => {
-  await page.goto("http://localhost:8000/");
   await page.getByPlaceholder("Enter command here!").fill("mode verbose");
   await page.getByRole("button", { name: "Submitted Briefly" }).click();
   await expect(
@@ -102,7 +92,6 @@ test("test viewing not existing csv file", async ({ page }) => {
 });
 
 test("test view", async ({ page }) => {
-  await page.goto("http://localhost:8000/");
   await page.getByPlaceholder("Enter command here!").fill("load people.csv");
   await page.getByRole("button", { name: "Submitted Briefly" }).click();
   await expect(
@@ -111,22 +100,9 @@ test("test view", async ({ page }) => {
   await page.getByPlaceholder("Enter command here!").fill("view people.csv");
   await page.getByRole("button", { name: "Submitted Briefly" }).click();
 
-  await page.waitForSelector(".repl-history");
-  const tableData = await page.evaluate(() => {
-    const rows = Array.from(document.querySelectorAll(".repl-history tr"));
-    return rows.map((row) =>
-      Array.from(row.querySelectorAll("td")).map((cell) => cell.textContent)
-    );
-  });
-  const expectedData = [
-    ["Name", "Age", "City"],
-    ["Alice", "25", "New York"],
-    ["Bob", "30", "Chicago"],
-    ["Charlie", "35", "Los Angeles"],
-    ["Percy", "26", "New York"],
-  ];
-
-  expect(tableData).toEqual(expectedData);
+  await expect(page.getByLabel("history-div")).toContainText([
+    "Alice25New York",
+  ]);
 
   await page.getByPlaceholder("Enter command here!").fill("view movies.csv");
   await page.getByRole("button", { name: "Submitted Briefly" }).click();
@@ -136,7 +112,6 @@ test("test view", async ({ page }) => {
 });
 
 test("search", async ({ page }) => {
-  await page.goto("http://localhost:8000/");
   await page.getByPlaceholder("Enter command here!").fill("load people.csv");
   await page.getByRole("button", { name: "Submitted Briefly" }).click();
   await expect(
@@ -150,8 +125,6 @@ test("search", async ({ page }) => {
 });
 
 test("test load -> view -> load another -> view another", async ({ page }) => {
-  await page.goto("http://localhost:8000/");
-
   await page.getByPlaceholder("Enter command here!").fill("load people.csv");
   await page.getByRole("button", { name: "Submitted Briefly" }).click();
 

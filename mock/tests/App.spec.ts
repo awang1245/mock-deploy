7873e5c6d72866d51187374d5b509486b01810c6
Jiwon <jiwon_yoo@brown.edu>
2023-10-14 00:08:23 -0400

    feat: testing suite done! only need to test with diverse datasets

--- mock/tests/App.spec.ts
+++ mock/tests/App.spec.ts
@@ -60,12 +60,36 @@ test("load success message", async ({ page }) => {
   ).toBeVisible();
 });
 
-test("view successful message", async ({ page }) => {
+test("load fail message", async ({ page }) => {
+  await page.getByPlaceholder("Enter command here!").fill("load fake.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await expect(
+    page.getByText("Output: Error: File fake.csv not found")
+  ).toBeVisible();
+});
+
+test("view success message", async ({ page }) => {
   await page.getByPlaceholder("Enter command here!").fill("load people.csv");
   await page.getByRole("button", { name: "Submitted Briefly" }).click();
   await page.getByPlaceholder("Enter command here!").fill("view people.csv");
   await page.getByRole("button", { name: "Submitted Briefly" }).click();
-  //how to test for checking whether the table is there....
+  await expect(page.getByLabel("history-div")).toContainText([
+    "NameAgeCityAlice25New YorkBob30ChicagoCharlie35Los AngelesPercy26New York",
+  ]);
+});
+
+test("view success message after success load and fail load", async ({
+  page,
+}) => {
+  await page.getByPlaceholder("Enter command here!").fill("load people.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await page.getByPlaceholder("Enter command here!").fill("load fake.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await page.getByPlaceholder("Enter command here!").fill("view people.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await expect(page.getByLabel("history-div")).toContainText([
+    "NameAgeCityAlice25New YorkBob30ChicagoCharlie35Los AngelesPercy26New York",
+  ]);
 });
 
 test("test view before load", async ({ page }) => {
@@ -76,34 +100,19 @@ test("test view before load", async ({ page }) => {
   ).toBeVisible();
 });
 
-test("test viewing not existing csv file", async ({ page }) => {
-  await page.getByPlaceholder("Enter command here!").fill("mode verbose");
+test("test view without filepath", async ({ page }) => {
+  await page.getByPlaceholder("Enter command here!").fill("load people.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await page.getByPlaceholder("Enter command here!").fill("view");
   await page.getByRole("button", { name: "Submitted Briefly" }).click();
   await expect(
-    page.getByText("Command: mode verbose Output: Mode set to Verbose")
-  ).toBeVisible();
-  await page.getByPlaceholder("Enter command here!").fill("load fake.csv");
-  await page.getByRole("button", { name: "Submitted Verbosely" }).click();
-  await expect(
-    page.getByText(
-      "Command: load fake.csv Output: Error: File fake.csv not found"
-    )
+    page.getByText("Output: Error: Please specify the dataset to view.")
   ).toBeVisible();
 });
 
-test("test view", async ({ page }) => {
+test("test load -> view different file", async ({ page }) => {
   await page.getByPlaceholder("Enter command here!").fill("load people.csv");
   await page.getByRole("button", { name: "Submitted Briefly" }).click();
-  await expect(
-    page.getByText("Output: Successfully loaded file people.csv")
-  ).toBeVisible();
-  await page.getByPlaceholder("Enter command here!").fill("view people.csv");
-  await page.getByRole("button", { name: "Submitted Briefly" }).click();
-
-  await expect(page.getByLabel("history-div")).toContainText([
-    "Alice25New York",
-  ]);
-
   await page.getByPlaceholder("Enter command here!").fill("view movies.csv");
   await page.getByRole("button", { name: "Submitted Briefly" }).click();
   await expect(
@@ -124,7 +133,108 @@ test("search", async ({ page }) => {
   ]);
 });
 
-test("test load -> view -> load another -> view another", async ({ page }) => {
+test("search returning multiple results", async ({ page }) => {
+  await page.getByPlaceholder("Enter command here!").fill("load movies.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await page.getByPlaceholder("Enter command here!").fill("search Year 2023");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await expect(page.getByLabel("history-div")).toContainText([
+    "Barbie2023Greta GerwigOppenheimer2023Christopher Nolan",
+  ]);
+});
+
+test("search without query", async ({ page }) => {
+  await page.getByPlaceholder("Enter command here!").fill("load people.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await page.getByPlaceholder("Enter command here!").fill("search");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await expect(
+    page.getByText(
+      "Output: Error: Please indicate the column and value to search"
+    )
+  ).toBeVisible();
+});
+
+test("search unfound query", async ({ page }) => {
+  await page.getByPlaceholder("Enter command here!").fill("load people.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await page.getByPlaceholder("Enter command here!").fill("search Name Jiwon");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await expect(
+    page.getByText(
+      "Output: Error: No search results matches the search value. Current loaded dataset: people.csv"
+    )
+  ).toBeVisible();
+});
+
+test("search before load", async ({ page }) => {
+  await page.getByPlaceholder("Enter command here!").fill("search Name Alice");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await expect(
+    page.getByText("Output: Error: No dataset loaded.")
+  ).toBeVisible();
+});
+
+test("invalid command inputted", async ({ page }) => {
+  await page.getByPlaceholder("Enter command here!").fill("hihi");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await expect(page.getByText("Output: Error: Invalid command.")).toBeVisible();
+});
+
+test("integration: load -> view -> search", async ({ page }) => {
+  await page.getByPlaceholder("Enter command here!").fill("load people.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await page.getByPlaceholder("Enter command here!").fill("view people.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await expect(page.getByLabel("history-div")).toContainText([
+    "NameAgeCityAlice25New YorkBob30ChicagoCharlie35Los AngelesPercy26New York",
+  ]);
+  await page.getByPlaceholder("Enter command here!").fill("search Name Alice");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await expect(page.getByLabel("history-div")).toContainText([
+    "Alice25New York",
+  ]);
+});
+
+test("integration: load file 1 -> load file 2 -> view file 1", async ({
+  page,
+}) => {
+  await page.getByPlaceholder("Enter command here!").fill("load people.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await page.getByPlaceholder("Enter command here!").fill("load movies.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await page.getByPlaceholder("Enter command here!").fill("view people.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await expect(
+    page.getByText("Output: Error: This dataset is not loaded to be viewed.")
+  ).toBeVisible();
+});
+
+test("integration: test load -> view -> view different file", async ({
+  page,
+}) => {
+  await page.getByPlaceholder("Enter command here!").fill("load people.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await expect(
+    page.getByText("Output: Successfully loaded file people.csv")
+  ).toBeVisible();
+  await page.getByPlaceholder("Enter command here!").fill("view people.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+
+  await expect(page.getByLabel("history-div")).toContainText([
+    "Alice25New York",
+  ]);
+
+  await page.getByPlaceholder("Enter command here!").fill("view movies.csv");
+  await page.getByRole("button", { name: "Submitted Briefly" }).click();
+  await expect(
+    page.getByText("Output: Error: This dataset is not loaded to be viewed.")
+  ).toBeVisible();
+});
+
+test("integration: test load -> view -> load another -> view another", async ({
+  page,
+}) => {
   await page.getByPlaceholder("Enter command here!").fill("load people.csv");
   await page.getByRole("button", { name: "Submitted Briefly" }).click();
 
